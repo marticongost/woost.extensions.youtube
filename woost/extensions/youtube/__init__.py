865b436ce31e1780db53c551188d8e33b38e04bd
Oriol Escolar <oriol.escolar@whads.com>
2019-06-18 17:55:43 +0200

    Bugfix: the videoplayersettings module was not being imported

--- woost/extensions/youtube/__init__.py
+++ woost/extensions/youtube/__init__.py
@@ -3,7 +3,7 @@
 
 .. moduleauthor:: Martí Congost <marti.congost@whads.com>
 """
-from . import settings, admin
+from . import settings, admin, videoplayersettings
 from .youtubevideo import YouTubeVideo
 from .youtubevideorenderer import YouTubeVideoRenderer
 from .installation import install
